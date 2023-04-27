78e8fa8409386dbc56a9ae5f103e5725c52c39af
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-04-27 20:35:38 +0200

    all of it

---
+++ app.js
@@ -0,0 +1,46 @@
+"use strict";
+
+window.addEventListener("load", initApp);
+
+let hairdresserSelector = 0;
+
+function initApp() {
+  document
+    .querySelector("#hairdresser-selected")
+    .addEventListener("change", modeSelected);
+}
+
+function modeSelected() {
+  const selectedMode = this.value;
+  changeOfMode(selectedMode);
+  setDOM();
+}
+
+function changeOfMode(selected) {
+  if (selected == "1") {
+    hairdresserSelector = 1;
+  } else if (selected == "2") {
+    hairdresserSelector = 2;
+  } else if (selected == "3") {
+    hairdresserSelector = 3;
+  } else if (selected == "4") {
+    hairdresserSelector = 4;
+  }
+  console.log(hairdresserSelector);
+}
+
+function setDOM() {
+  let htmlDOM;
+  if (hairdresserSelector == 1) {
+    htmlDOM = `<select id="hairdresser1">
+     <option>Hey</option>
+     <option>Jude</option>
+     </select>`;
+  } else if (hairdresserSelector == 2) {
+    htmlDOM = `<select id="hairdresser2">
+     <option>Yellow</option>
+     <option>Submarine</option>
+     </select>`;
+  }
+  document.querySelector("#forms-div").insertAdjacentHTML("beforeend", htmlDOM);
+}
